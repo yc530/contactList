6d300b0287ff0da09516491b78e87b03a3fd5b65
LRK-2099 <143131113+LRK-2099@users.noreply.github.com>
2023-10-12 15:45:35 -0400

    made functionality fro API

--- src/components./ContactList.jsx
+++ src/components./ContactList.jsx
@@ -1,17 +1,31 @@
 //Create a component, and name it ContactList
 
 import React, {useState} from "react"; 
-
+import ContactRow from "./ContactRow";
+import {useEffect} from 'react';
 
 export default function ContactList() { 
-    const [contacts, setContacts] = useState(dummyContacts)
-
     const dummyContacts = [
         { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
         { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
         { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
        ];
+       const [contacts, setContacts] = useState(dummyContacts)
     console.log("Contacts",contacts)
+    useEffect(()=>{
+        async function fetchContacts() {
+            try {
+              const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users')
+            const result = await response.json()
+       setContacts(result)
+       console.log("contacts", result)
+        } catch (error) {
+              console.error(error);
+            }
+          }
+          fetchContacts()
+        }, []);
+       
   return ( 
         <table>
           <thead>
@@ -26,8 +40,10 @@ export default function ContactList() {
               <td>Phone</td>
             </tr>
             {
-        
-            } 
+        contacts.map((contact) => {
+            return <ContactRow key={contact.id} contact={contact} />;
+          })}
+
           </tbody>
         </table>
     ); 
