28c0c39da53dc4d9dd90a8af4206a6e5637af75a
LRK-2099 <143131113+LRK-2099@users.noreply.github.com>
2023-10-12 15:00:50 -0400

    new changes and arrangments

--- src/components./ContactList.jsx
+++ src/components./ContactList.jsx
@@ -1,16 +1,17 @@
 //Create a component, and name it ContactList
 
-import React from "react"; 
-const [contacts, setContacts] = useState(dummyContacts)
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-   ];
+import React, {useState} from "react"; 
 
 
 export default function ContactList() { 
     const [contacts, setContacts] = useState(dummyContacts)
+
+    const dummyContacts = [
+        { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+        { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+        { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+       ];
+    console.log("Contacts",contacts)
   return ( 
         <table>
           <thead>
