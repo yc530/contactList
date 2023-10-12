6cf7d3b2f53a21c284dbe8d10c4ad992764edf08
yc530 <yoonachoi530@gmail.com>
2023-10-12 16:17:01 -0400

    fixed syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import ContactList from './components./ContactList'
 import SelectedContact from "./components/SelectedContact";
 
 function App() {
-  const [count, setCount] = useState(0)
   const [selectedContactId, setSelectedContactId] = useState(null);
   
   return (
     <>
     {selectedContactId ? (
         <div>Selected Contact View</div>
-      ):
+      ):(
 <ContactList /> 
     )}
     </>
