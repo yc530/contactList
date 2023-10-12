3e7986f39ddc378593e0e56d11e94286a0e50979
LRK-2099 <143131113+LRK-2099@users.noreply.github.com>
2023-10-12 14:53:57 -0400

    moved diffrent commands

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import ContactList from './components./ContactList'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+<ContactList /> 
+
     </>
   )
 }
